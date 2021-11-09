b86d9a901ae1c31bcc61fe262749873fffbcf8a7
GIMIAIN <sasha.sasha.korolev2003@gmail.com>
2021-11-09 18:57:22 +0700

    Try

--- test_application.py
+++ test_application.py
@@ -1,8 +1,8 @@
 from application import application
-from application import app
+#from application import app
 if application == "Hello world":
     try:
-        application = app
+        application = 1/0
     except ZeroDivisionError as err:
         print("Ошибка - деление на 0")
 else:
